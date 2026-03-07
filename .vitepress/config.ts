45200452f4f9726ed04c6f5fcf1c5a3ab2060fe3
kwiamwis <123845951+kwiamwis@users.noreply.github.com>
2026-03-07 01:46:08 -0500

    Add files via upload

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -13,35 +13,36 @@ export default defineConfig({
 //social
     socialLinks: [
       { icon: 'discord', link: 'https://discord.aalenia.com' },
-      { icon: 'trello', link: 'https://trello.aalenia.com' },
+//      { icon: 'trello', link: 'https://trello.aalenia.com' },
       { icon: 'github', link: 'https://github.com/kwiamwis' },
       { icon: 'paypal', link: 'https://www.paypal.me/kwiamwis' },
+      { icon: 'youtube', link: 'https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html' },
     ],
 //nav
     nav: [
-      { text: 'Mods', link: '/mods/utilities' },
-      { text: 'Servers', link: '/servers/survival' },
+      { text: 'Mods & Applications', link: '/potapps/utilities' },
+      { text: 'Servers', link: '/potservers/survival' },
       { text: 'Guidelines', link: '/guidelines' },
       { text: 'Bug Reports', link: '/bugs' },
       { text: 'About', link: '/about' },
       {
         text: 'Resources',
         items: [
-          {
-            text: 'PoT Info',
-            items: [
-              { text: 'PoT Character Stats', link: 'https://guides.gsh-servers.com/path-of-titans/guides/curve-overrides/' },
-              { text: 'AG Status', link: 'https://status.alderongames.com/' },
-            ]
-          },
-          {
-            text: 'PoT Support',
-            items: [
-              { text: 'AG Bug Reports', link: 'https://bugtracker.alderongames.com/' },
-              { text: 'AG Support', link: 'https://support.alderongames.com/' },
-              { text: 'Bug Tracker Skins', link: 'https://support.alderongames.com/hc/en-us/articles/34352973267481-How-do-I-get-a-Bugtracker-skin' },
-            ]
-          },
+          // {
+          //   text: 'PoT Info',
+          //   items: [
+          //     { text: 'PoT Character Stats', link: 'https://guides.gsh-servers.com/path-of-titans/guides/curve-overrides/' },
+          //     { text: 'AG Status', link: 'https://status.alderongames.com/' },
+          //   ]
+          // },
+          // {
+          //   text: 'PoT Support',
+          //   items: [
+          //     { text: 'AG Bug Reports', link: 'https://bugtracker.alderongames.com/' },
+          //     { text: 'AG Support', link: 'https://support.alderongames.com/' },
+          //     { text: 'Bug Tracker Skins', link: 'https://support.alderongames.com/hc/en-us/articles/34352973267481-How-do-I-get-a-Bugtracker-skin' },
+          //   ]
+          // },
           {
             text: 'PoT Dev',
             items: [
@@ -49,13 +50,13 @@ export default defineConfig({
               { text: 'AG Modding', link: 'https://ugc.alderongames.com/login' },
             ]
           },
-          {
-            text: 'Paleohistory',
-            items: [
-              { text: 'Prehistoric Globes', link: 'https://dinosaurpictures.org/ancient-earth' },
-              { text: 'Stratigraphy', link: 'https://stratigraphy.org/ICSchart/ChronostratChart2024-12.pdf' },
-            ]
-          },
+          // {
+          //   text: 'Paleohistory',
+          //   items: [
+          //     { text: 'Prehistoric Globes', link: 'https://dinosaurpictures.org/ancient-earth' },
+          //     { text: 'Stratigraphy', link: 'https://stratigraphy.org/ICSchart/ChronostratChart2024-12.pdf' },
+          //   ]
+          // },
           {
             text: 'Discord Info',
             items: [
@@ -63,7 +64,6 @@ export default defineConfig({
               { text: 'Timestamps', link: 'https://sesh.fyi/timestamp/' },
               { text: 'Styling', link: 'https://fsymbols.com/letters/' },
               { text: 'Markdown', link: 'https://www.markdownguide.org/tools/discord/' },
-              { text: 'Giveaways', link: 'https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html' },
             ]
           },
           {
@@ -79,48 +79,53 @@ export default defineConfig({
 // sidebar
     sidebar: {
 // mods
-      '/mods/': [
+      '/potapps/': [
         {
-          text: 'Mods',
+          text: 'Path of Titans Mods',
           items:[
             {
               text: 'Utilities',
-              link: '/mods/utilities',
-            },
-            {
-              text: 'Relay',
-              link: '/mods/relay',
+              link: '/potapps/utilities',
             },
             {
               text: 'Flora',
-              link: '/mods/flora',
-            },
-            {
-              text: 'Fauna',
-              link: '/mods/fauna',
+              link: '/potapps/flora',
             },
           ]
         },
         {
-          text: 'Applications',
+          text: 'Path of Titans Applications',
           items:[
             {
               text: 'DupaBot',
-              link: '/mods/dupabot',
+              link: '/potapps/dupabot',
             },
           ]
         },
       ],
 // servers
-      '/servers/': [
+      '/potservers/': [
         {
-          text: 'Survival',
-          link: '/servers/survival',
+          text: 'Path of Titans Servers',
+          items:[
+            {
+              text: 'Survival',
+              link: '/potservers/survival',
+            },
+            {
+              text: 'PvP',
+              link: '/potservers/pvp',
+            },
+          ]
         },
-        {
-          text: 'PvP',
-          link: '/servers/pvp',
-        },
+        // {
+        //   text: 'Survival',
+        //   link: '/potservers/survival',
+        // },
+        // {
+        //   text: 'PvP',
+        //   link: '/potservers/pvp',
+        // },
       ],
     },
   }
